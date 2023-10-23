7dd17d163a2a543a1abd9003f2097897765d346a
Walexero <ayanladeolawale@gmail.com>
2023-10-23 13:09:46 -0400

    Added makemigrations to docker-compose-deploy run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,11 +2,10 @@
 
 set -e
 
-echo "$DEV thee dev var val"
 if [ $DEV != "true" ]; then
-    echo "Triggered Build Logic"
     python manage.py wait_for_db
     python manage.py collectstatic --noinput
+    python manage.py makemigrations
     python manage.py migrate
 
     uwsgi --socket :9090 --workers 4 --master --enable-threads --module app.wsgi
