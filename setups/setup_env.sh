2eedc3baf274e5454e55c7393e0b32a052be3616
Walexero <ayanladeolawale@gmail.com>
2023-10-23 08:06:20 -0400

    added no cache to build

--- setups/setup_env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-echo DB_NAME=$DB_NAME >> .env
-echo DB_USER=$DB_USER >> .env
-echo DB_PASS=$DB_PASS >> .env
-echo SECRET_KEY=$SECRET_KEY >> .env
-echo ALLOWED_HOSTS=$HOST_DNS >> .env
